87d0e6b29eb11fd832b111fda37fd9d9dfc68f4e
tranvula <vu@micromacro.com.au>
2018-09-13 17:14:19 +1000

    Test

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -9,6 +9,7 @@ namespace ConsoleApp2
             string test = "";
             string test2 = "";
             string test3 = "";
+            string test4 = "";
             Console.WriteLine("Hello World!");
         }
     }
